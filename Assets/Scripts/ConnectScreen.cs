031870729e6774290739b03254b96e232ff0619a
Sami Habib <puggsoy@gmail.com>
2025-09-24 10:31:59 +1200

    Completed state

--- Assets/Scripts/ConnectScreen.cs
+++ Assets/Scripts/ConnectScreen.cs
@@ -1,4 +1,13 @@
+using FishNet;
+using FishNet.Connection;
+using FishNet.Transporting.UTP;
+using System.Collections.Generic;
 using TMPro;
+using Unity.Networking.Transport.Relay;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -24,6 +33,25 @@ public class ConnectScreen : MonoBehaviour
 	[SerializeField]
 	private TMP_InputField m_joinInputField = null;
 
+	private void Start()
+	{
+		m_buttonContainer.SetActive(false);
+
+		InitializeService();
+	}
+
+	private async void InitializeService()
+	{
+		await UnityServices.InitializeAsync();
+
+		if (!AuthenticationService.Instance.IsSignedIn)
+		{
+			await AuthenticationService.Instance.SignInAnonymouslyAsync();
+		}
+
+		m_buttonContainer.SetActive(true);
+	}
+
 	public void SoloClick()
 	{
 		SceneManager.LoadScene("MainScene");
@@ -33,7 +61,56 @@ public class ConnectScreen : MonoBehaviour
 	{
 		m_buttonContainer.SetActive(false);
 		m_hostContainer.SetActive(true);
+
+		SetupHost();
+	}
+
+	private async void SetupHost()
+	{
+		m_codeText.text = "Getting code...";
+
+		Allocation allocation = await RelayService.Instance.CreateAllocationAsync(2);
+		string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+
+		UnityTransport transport = InstanceFinder.TransportManager.GetTransport<UnityTransport>();
+		RelayServerData relayServerData = AllocationUtils.ToRelayServerData(allocation, "dtls");
+		transport.SetRelayServerData(relayServerData);
+
+		if (!InstanceFinder.ServerManager.StartConnection())
+		{
+			//error handling
+			return;
+		}
+
+		if (!InstanceFinder.ClientManager.StartConnection())
+		{
+			//error handling
+			return;
+		}
+
+		m_codeText.text = joinCode;
+
 		m_backBtn.SetActive(true);
+
+		InstanceFinder.ServerManager.OnAuthenticationResult += ServerOnAuthenticationResult;
+	}
+
+	private void ServerOnAuthenticationResult(FishNet.Connection.NetworkConnection connection, bool authenticated)
+	{
+		Dictionary<int, NetworkConnection> clients = InstanceFinder.ServerManager.Clients;
+
+		if (clients.Count == 2)
+		{
+			foreach (NetworkConnection client in clients.Values)
+			{
+				if (!client.IsAuthenticated)
+				{
+					return;
+				}
+			}
+
+			SceneManager.LoadScene("MainScene");
+		}
 	}
 
 	public void JoinClick()
@@ -45,11 +122,44 @@ public class ConnectScreen : MonoBehaviour
 
 	public void SubmitClick()
 	{
-		Debug.Log("Submitting isn't implemented yet!");
+		m_backBtn.SetActive(false);
+
+		JoinGame();
+	}
+
+	public async void JoinGame()
+	{
+		JoinAllocation allocation = await RelayService.Instance.JoinAllocationAsync(m_joinInputField.text);
+
+		if (allocation == null)
+		{
+			//error handling
+			return;
+		}
+
+		UnityTransport transport = InstanceFinder.TransportManager.GetTransport<UnityTransport>();
+		RelayServerData relayServerData = AllocationUtils.ToRelayServerData(allocation, "dtls");
+		transport.SetRelayServerData(relayServerData);
+
+		InstanceFinder.ClientManager.OnAuthenticated += ClientOnAuthenticated;
+		
+		if (!InstanceFinder.ClientManager.StartConnection())
+		{
+			//error handling
+			InstanceFinder.ClientManager.OnAuthenticated -= ClientOnAuthenticated;
+		}
+	}
+
+	private void ClientOnAuthenticated()
+	{
+		SceneManager.LoadScene("MainScene");
 	}
 
 	public void BackClick()
 	{
+		InstanceFinder.ServerManager.StopConnection(true);
+		InstanceFinder.ClientManager.StopConnection();
+
 		m_hostContainer.SetActive(false);
 		m_joinContainer.SetActive(false);
 		m_buttonContainer.SetActive(true);
--- Assets/Scripts/OptionUI.cs
+++ Assets/Scripts/OptionUI.cs
@@ -1,8 +1,18 @@
+using FishNet;
+using FishNet.Broadcast;
+using FishNet.Connection;
+using FishNet.Transporting;
 using System;
 using UnityEngine;
 
 public class OptionUI : MonoBehaviour
 {
+	private struct SelectBroadcast : IBroadcast
+	{
+		public string ObjectName;
+		public bool SentByHost;
+	}
+
 	public event Action<OptionUI, bool> OnSelected = null;
 
 	[SerializeField]
@@ -13,9 +23,50 @@ public class OptionUI : MonoBehaviour
 
 	//==================================================
 
+	private void OnEnable()
+	{
+		InstanceFinder.ServerManager.RegisterBroadcast<SelectBroadcast>(Server_SelectBroadcastHandler);
+		InstanceFinder.ClientManager.RegisterBroadcast<SelectBroadcast>(Client_SelectBroadcastHandler);
+	}
+
+	private void Client_SelectBroadcastHandler(SelectBroadcast broadcast, Channel channel)
+	{
+		SelectBroadcastHandler(broadcast);
+	}
+
+	private void Server_SelectBroadcastHandler(NetworkConnection connection, SelectBroadcast broadcast, Channel channel)
+	{
+		SelectBroadcastHandler(broadcast);
+	}
+
+	private void SelectBroadcastHandler(SelectBroadcast broadcast)
+	{
+		if (broadcast.ObjectName == name)
+		{
+			Select(broadcast.SentByHost);
+		}
+	}
+
 	public void OnClick()
 	{
-		Select(true);
+		bool isHost = InstanceFinder.IsHostStarted;
+
+		Select(isHost);
+
+		SelectBroadcast broadcast = new SelectBroadcast()
+		{
+			ObjectName = name,
+			SentByHost = isHost
+		};
+
+		if (isHost)
+		{
+			InstanceFinder.ServerManager.Broadcast(broadcast);
+		}
+		else
+		{
+			InstanceFinder.ClientManager.Broadcast(broadcast);
+		}
 	}
 
 	public void Select(bool host)
@@ -32,5 +83,11 @@ public class OptionUI : MonoBehaviour
 		GameObject text = host ? m_hostText : m_clientText;
 
 		text.SetActive(false);
+	}
+
+	private void OnDisable()
+	{
+		InstanceFinder.ServerManager.UnregisterBroadcast<SelectBroadcast>(Server_SelectBroadcastHandler);
+		InstanceFinder.ClientManager.UnregisterBroadcast<SelectBroadcast>(Client_SelectBroadcastHandler);
 	}
 }
